225c923217276a066cdd1c14a7c1b43d88b844bb
Arghadeep Dutta <229253553+arghadeep2508@users.noreply.github.com>
2026-04-13 15:22:23 +0530

    Add Home component for GPS Safety Tracker

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>GPS Safety Tracker</h1>
+      <a href="/register">Register</a><br />
+      <a href="/login">Login</a>
+    </div>
+  )
+}
