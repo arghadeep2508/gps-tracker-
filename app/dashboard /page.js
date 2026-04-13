3a9754c8a9aa0116d650bc4f62dab4055776bc94
Arghadeep Dutta <229253553+arghadeep2508@users.noreply.github.com>
2026-04-13 15:52:52 +0530

    Create page.js

---
+++ app/dashboard /page.js
@@ -0,0 +1,69 @@
+'use client'
+import { useEffect, useState } from 'react'
+import { supabase } from '../../lib/supabaseClient'
+
+export default function Dashboard() {
+  const [profile, setProfile] = useState(null)
+
+  useEffect(() => {
+    const loadProfile = async () => {
+      const { data: { user } } = await supabase.auth.getUser()
+
+      const { data } = await supabase
+        .from('profiles')
+        .select('*')
+        .eq('id', user.id)
+        .single()
+
+      setProfile(data)
+    }
+
+    loadProfile()
+  }, [])
+
+  const sendSOS = () => {
+    if (!navigator.geolocation) {
+      alert("GPS not supported")
+      return
+    }
+
+    navigator.geolocation.getCurrentPosition((pos) => {
+      const lat = pos.coords.latitude
+      const lon = pos.coords.longitude
+
+      const msg = `EMERGENCY 🚨
+Name: ${profile.name}
+Phone: ${profile.phone}
+Age: ${profile.age}
+Health: ${profile.health}
+
+https://maps.google.com/?q=${lat},${lon}`
+
+      window.location.href =
+        `sms:${profile.emergency_phone}?body=${encodeURIComponent(msg)}`
+    })
+  }
+
+  if (!profile) return <p>Loading...</p>
+
+  return (
+    <div>
+      <h2>Dashboard</h2>
+
+      <p>Emergency Contact: {profile.emergency_name}</p>
+      <p>{profile.emergency_phone}</p>
+
+      <button
+        style={{
+          background: 'red',
+          color: 'white',
+          padding: '20px',
+          fontSize: '20px'
+        }}
+        onClick={sendSOS}
+      >
+        SOS
+      </button>
+    </div>
+  )
+}
