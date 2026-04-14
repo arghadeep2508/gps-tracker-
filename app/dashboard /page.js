bb1ecedfb21c3e12a0eff5269e3970dea86309c4
Arghadeep Dutta <229253553+arghadeep2508@users.noreply.github.com>
2026-04-14 13:32:30 +0530

    Update page.js

--- app/dashboard /page.js
+++ app/dashboard /page.js
@@ -6,7 +6,7 @@ export default function Dashboard() {
   const [profile, setProfile] = useState(null)
 
   useEffect(() => {
-    const loadProfile = async () => {
+    const load = async () => {
       const { data: { user } } = await supabase.auth.getUser()
 
       const { data } = await supabase
@@ -18,15 +18,10 @@ export default function Dashboard() {
       setProfile(data)
     }
 
-    loadProfile()
+    load()
   }, [])
 
   const sendSOS = () => {
-    if (!navigator.geolocation) {
-      alert("GPS not supported")
-      return
-    }
-
     navigator.geolocation.getCurrentPosition((pos) => {
       const lat = pos.coords.latitude
       const lon = pos.coords.longitude
@@ -47,22 +42,21 @@ https://maps.google.com/?q=${lat},${lon}`
   if (!profile) return <p>Loading...</p>
 
   return (
-    <div>
-      <h2>Dashboard</h2>
+    <div className="bg-slate-900 p-6 rounded-2xl w-[350px] text-center shadow-xl">
+      <h2 className="text-xl font-bold mb-4 text-blue-400">
+        Dashboard
+      </h2>
 
-      <p>Emergency Contact: {profile.emergency_name}</p>
-      <p>{profile.emergency_phone}</p>
+      <p className="mb-1">Emergency Contact</p>
+      <p className="mb-4 text-gray-400">
+        {profile.emergency_name} ({profile.emergency_phone})
+      </p>
 
       <button
-        style={{
-          background: 'red',
-          color: 'white',
-          padding: '20px',
-          fontSize: '20px'
-        }}
         onClick={sendSOS}
+        className="w-full bg-red-600 p-4 rounded-full text-xl font-bold hover:bg-red-700 animate-pulse"
       >
-        SOS
+        🚨 SOS
       </button>
     </div>
   )
